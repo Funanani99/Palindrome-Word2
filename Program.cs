e6b3d893d184aa4a6f46563c970d077e5c75b7ff
Dell-User <Dell-User@DESKTOP-GCMBBUK>
2023-10-13 07:22:37 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome_Word2
+{
+    internal class PalindromeChecker
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a word: ");
+            string input = Console.ReadLine();
+
+            if (IsWordPalindrome(input))
+            {
+                Console.WriteLine($"The word '{input}' is a fascinating palindrome!");
+            }
+            else
+            {
+                Console.WriteLine($"The word '{input}' is not a palindrome.");
+            }
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadLine();
+        }
+
+        static bool IsWordPalindrome(string word)
+        {
+            word = word.ToLower(); // Make it case-insensitive
+            for (int i = 0, j = word.Length - 1; i < j; i++, j--)
+            {
+                if (word[i] != word[j])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
